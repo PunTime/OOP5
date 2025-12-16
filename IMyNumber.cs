3417aabee7ce9ac692fe0044c51b2d05aa43d5e7
PunTime <borovikrostislav641@gmail.com>
2025-12-16 20:07:02 +0200

    Add files via upload

---
+++ IMyNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP5
+{
+    /*Оголошення дженерик-інтерфейсу для арифметичних операцій*/
+    public interface IMyNumber<T> where T : IMyNumber<T>
+    {
+        /*Метод додавання: повертає результат того ж типу*/
+        T Add(T b);
+        /*Метод віднімання*/
+        T Subtract(T b);
+        /*Метод множення*/
+        T Multiply(T b);
+        /*Метод ділення*/
+        T Divide(T b);
+    }
+}
