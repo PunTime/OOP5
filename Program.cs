2393a68e5b4380b2f86600fc951fd040c1f94eb6
PunTime <borovikrostislav641@gmail.com>
2025-12-16 19:33:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace OOP5
+{
+    static class Program
+    {
+        /*Вказує, що головний потік програми використовує однопоточну модель COM (Single-Threaded Apartment)*/
+        [STAThread]
+        static void Main()
+        {
+            /*Активує сучасні візуальні стилі для елементів керування Windows Forms*/
+            Application.EnableVisualStyles();
+            /*Встановлює режим рендерингу тексту для елементів керування Windows Forms*/
+            Application.SetCompatibleTextRenderingDefault(false);
+            /*Запускає головну форму додатку Form1 і починає цикл обробки повідомлень Windows*/
+            Application.Run(new Form1());
+        }
+    }
+}
